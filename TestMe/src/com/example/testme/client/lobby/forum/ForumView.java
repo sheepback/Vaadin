ae455edb978729e7f82062de13a22d2f51956865
Alex <Alex@Alex-PC.multi.box>
2016-02-28 18:53:17 +0100

    *

--- TestMe/src/com/example/testme/client/lobby/forum/ForumView.java
+++ TestMe/src/com/example/testme/client/lobby/forum/ForumView.java
@@ -2,12 +2,16 @@ package com.example.testme.client.lobby.forum;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.example.testme.client.View;
 import com.example.testme.client.lobby.forum.ForumPresenter.Display;
 import com.example.testme.client.lobby.forum.game.Field;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.Reindeer;
 
@@ -15,37 +19,62 @@ import com.vaadin.ui.themes.Reindeer;
  * @author Alexander Thomas
  * @date 07.02.2016
  */
-public class ForumView implements Display, View{
-	
+public class ForumView implements Display, View {
+
 	List<Field> fields = new ArrayList<Field>();
-	
+
+	List<HorizontalLayout> hz = new ArrayList<HorizontalLayout>();
+
 	VerticalLayout viewLayout;
 	
-	HorizontalLayout hz = new HorizontalLayout();
-			
-	public ForumView(){
+	HorizontalLayout statusbar;
+	
+	Label money;
+
+	Logger logger = Logger.getLogger("ForumView");
+
+	public ForumView() {
+		money = new Label("10");
+		statusbar = new HorizontalLayout(money);
 		
-		for (int i=0;i<10;i++) {
-			fields.add(new Field("Feld "+i));
-			hz.addComponent(fields.get(i));
-			hz.setComponentAlignment(fields.get(i), Alignment.MIDDLE_CENTER);
+		viewLayout = new VerticalLayout(statusbar);
+		for (int j = 0; j < 3; j++) {
+			hz.add(new HorizontalLayout());
+			viewLayout.addComponent(hz.get(j));
+			viewLayout.setComponentAlignment(hz.get(j), Alignment.MIDDLE_CENTER);
 		}
-		viewLayout = new VerticalLayout(hz);
-		viewLayout.setComponentAlignment(hz, Alignment.MIDDLE_CENTER);
+		for (int i = 0; i < 12; i++) {
+			fields.add(new Field("Feld " + i));
+			fields.get(i).setIcon(new ThemeResource("icons/gamepic.png"));
+			if (i < 4) {
+				hz.get(0).addComponent(fields.get(i));
+				hz.get(0).setComponentAlignment(fields.get(i),Alignment.MIDDLE_CENTER);
+			} else if (i < 8) {
+				hz.get(1).addComponent(fields.get(i));
+				hz.get(1).setComponentAlignment(fields.get(i),Alignment.MIDDLE_CENTER);
+			} else if (i < 12) {
+				hz.get(2).addComponent(fields.get(i));
+				hz.get(2).setComponentAlignment(fields.get(i),Alignment.MIDDLE_CENTER);
+			}
+		}
 		viewLayout.setStyleName(Reindeer.LAYOUT_BLACK);
 		viewLayout.setSizeFull();
 	}
-	
+
 	@Override
 	public ForumView getDisplay() {
 		return this;
 	}
-	
-	public VerticalLayout getViewLayout(){
+
+	public VerticalLayout getViewLayout() {
 		return this.viewLayout;
 	}
-	
-	public List<Field> getFields(){
+
+	public List<Field> getFields() {
 		return fields;
+	}
+	
+	public void setMoney(String money){
+		this.money.setValue(money);
 	}
 }
